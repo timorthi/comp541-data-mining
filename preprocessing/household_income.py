870e39c15ceb414837b3331c36295ac8dd4e153b
brentmercado <brent.mercado.566@my.csun.edu>
2018-10-02 15:46:44 -0700

    US household income preprocessing (#3)
    * Pre-process stage 1
    
    Import dataset and clean missing values
    
    * Create mean bin
    
    * complete Pre-Process Stage 2 (Data Cleaning)
    
    Created Bins to display the mean and median income in 3 different
    categories: Low, Medium, High. The bin names are: Mean-Binned,
    Median-Binned
    
    * Complete Pre-Process Stage 3 (Transformation)
    
    Z-Score was used for normalization
    
    * Stage 3 (Transformation) done for real...
    
    Converted categorical attributes Mean and Medium Binned
    into numerical attributes. This will make it easier for
    analysis.
    
    * Complete Income Pre-Processing
    
    Completed reduction step by eliminating unneccessary
    attributes. Exported new file 'pre_processed_income.csv'
    to the datasets folder
    
    * Finalize household_income pre-process
    
    Change export path to preprocessing folder
    
    * Now it's done for sure
    
    I hope so... Tim?
    
    * Tim??......
    
    * I don't trust you Tim.....

---
+++ preprocessing/household_income.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy
+import matplotlib.pyplot as plt
+
+importPath = "../datasets/US_Income_Kaggle.csv"
+
+############ IMPORT DATASET ######################
+df = pd.read_csv(importPath, encoding="ISO-8859-1 ")
+
+
+################ CHECK MISSING DATA ################
+# mark 0 values as missing or NaN
+# df[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]] = df[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]].replace(0, numpy.NaN)
+# print(df.isnull().sum())
+
+
+################# BINNING ###########################
+minMeanInt = int(min(df["Mean"]))
+maxMeanInt = int(max(df["Mean"]))
+minMedianInt = int(min(df["Median"]))
+maxMedianInt = int(max(df["Median"]))
+
+meanBinwidth = int((max(df["Mean"])-min(df["Mean"]))/4)
+meanBins = [minMeanInt, minMeanInt + meanBinwidth, maxMeanInt - meanBinwidth, maxMeanInt]
+
+medianBinwidth = int((max(df["Median"])-min(df["Median"]))/4)
+medianBins = [minMedianInt, minMedianInt + medianBinwidth, maxMedianInt - medianBinwidth, maxMedianInt]
+
+mean_group_names = ['MeanLow', 'MeanMedium', 'MeanHigh']
+median_group_names = ['MedianLow', 'MedianMedium', 'MedianHigh']
+
+df['Mean-Binned'] = pd.cut(df['Mean'], meanBins, labels=mean_group_names)
+df['Median-Binned'] = pd.cut(df['Median'], medianBins, labels=median_group_names)
+
+
+# # plot histogram
+# _ = plt.hist(df['Median'], bins=medianBins)
+# _ = plt.xlabel('Median Income')
+# _ = plt.ylabel('Number of Cities')
+# plt.show()
+
+#################### Z-SCORE #######################
+
+df["Mean"] = (df["Mean"] - df["Mean"].mean())/df["Mean"].std()
+df["Median"] = (df["Median"] - df["Median"].mean())/df["Median"].std()
+
+############## ATTRIBUTE CONSTRUCTION ##############
+
+mean_dummy = pd.get_dummies(df['Mean-Binned'])
+median_dummy = pd.get_dummies(df['Median-Binned'])
+
+df = pd.concat([df, mean_dummy], axis=1)
+df = pd.concat([df, median_dummy], axis=1)
+
+################### DATA REDUCTION ##################
+
+df.drop(['id', 'State_Code', 'State_Name', 'County', 'Place', 'Type', 'Primary', 'ALand', 'AWater'], axis=1, inplace=True)
+
+########## EXPORT PRE-PROCESSED DATASET ############
+
+df.to_csv(importPath)
