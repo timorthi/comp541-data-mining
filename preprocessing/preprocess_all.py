1fe9e96c788a9d6f8d6edde4f24d8bfa0a14cac0
Timothy Ng <timnjj@gmail.com>
2018-10-17 11:20:16 -0700

    Add script for running all preprocessing scripts

---
+++ preprocessing/preprocess_all.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    print('=== PROCESSING HOUSEHOLD INCOME DATA ===')
+    import household_income
+    print('=== DONE PROCESSING HOUSEHOLD INCOME DATA ===\n')
+
+    print('=== PROCESSING LA CRIME DATA ===')
+    import la_crime
+    print('=== DONE PROCESSING LA CRIME DATA ===\n')
+
+    print('=== PROCESSING TRIPADVISOR DATA ===')
+    import tripadvisorPreProc
+    print('=== DONE PROCESSING TRIPADVISOR DATA ===\n')
+
+    print('=== PROCESSING YELP DATA ===')
+    import yelp
+    print('=== DONE PROCESSING YELP DATA ===')
