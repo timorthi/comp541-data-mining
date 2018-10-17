4db6607afabcbbaccf47e21e2513a29ba2bd6866
Timothy Ng <timnjj@gmail.com>
2018-10-17 11:22:03 -0700

    Print out progress in preprocess driver

--- preprocessing/preprocess_all.py
+++ preprocessing/preprocess_all.py
@@ -1,16 +1,16 @@
 if __name__ == '__main__':
-    print('=== PROCESSING HOUSEHOLD INCOME DATA ===')
+    print('=== 1/4 PROCESSING HOUSEHOLD INCOME DATA ===')
     import household_income
     print('=== DONE PROCESSING HOUSEHOLD INCOME DATA ===\n')
 
-    print('=== PROCESSING LA CRIME DATA ===')
+    print('=== 2/4 PROCESSING LA CRIME DATA ===')
     import la_crime
     print('=== DONE PROCESSING LA CRIME DATA ===\n')
 
-    print('=== PROCESSING TRIPADVISOR DATA ===')
+    print('=== 3/4 PROCESSING TRIPADVISOR DATA ===')
     import tripadvisorPreProc
     print('=== DONE PROCESSING TRIPADVISOR DATA ===\n')
 
-    print('=== PROCESSING YELP DATA ===')
+    print('=== 4/4 PROCESSING YELP DATA ===')
     import yelp
     print('=== DONE PROCESSING YELP DATA ===')
